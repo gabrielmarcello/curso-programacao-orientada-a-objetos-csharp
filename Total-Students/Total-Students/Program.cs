9cb090cbbea81ecc246374babf33be608d272695
gabriel marcello <gabriel.marcellocf@gmail.com>
2023-09-23 15:01:35 -0300

    Adicionando exercício Total-Students

---
+++ Total-Students/Total-Students/Program.cs
@@ -0,0 +1,39 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        HashSet<int> courseA = new HashSet<int>();
+        HashSet<int> courseB = new HashSet<int>();
+        HashSet<int> courseC = new HashSet<int>();
+
+        Console.Write("How many students for course A? ");
+        int n = int.Parse(Console.ReadLine());
+        for (int i=0; i<n; i++)
+        {
+            int number = int.Parse(Console.ReadLine());
+            courseA.Add(number);
+        }
+
+        Console.Write("How many students for course B? ");
+        n = int.Parse(Console.ReadLine());
+        for (int i = 0; i < n; i++)
+        {
+            int number = int.Parse(Console.ReadLine());
+            courseB.Add(number);
+        }
+
+        Console.Write("How many students for course C? ");
+        n = int.Parse(Console.ReadLine());
+        for (int i = 0; i < n; i++)
+        {
+            int number = int.Parse(Console.ReadLine());
+            courseC.Add(number);
+        }
+
+        HashSet<int> all = new HashSet<int>(courseA);
+        all.UnionWith(courseB);
+        all.UnionWith(courseC);
+
+        Console.WriteLine("Total students: " + all.Count);
+    }
+}
